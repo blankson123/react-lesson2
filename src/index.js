a8adfa186c00ff2635b94c6e92099a903934d2f9
Richmond Blankson <mensahrichmondb@gmail.com>
2022-10-17 04:13:37 +0000

    completed lesson in section 5

---
+++ src/index.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import ReactDOM  from 'react-dom';
+
+
+class App extends React.Component{
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            lat: null,
+            errorMessage: '',
+        };
+
+        window.navigator.geolocation.getCurrentPosition(
+            position => {
+                this.setState({
+                    lat: position.coords.latitude
+                });
+            },
+            positionError => {
+                this.setState({
+                    errorMessage: positionError.message
+                });
+            }
+        );
+    }
+
+    render(){
+         if(this.state.errorMessage && !this.state.lat){
+             return <div>Error: {this.state.errorMessage}</div>
+         }
+
+         if(!this.state.errorMessage && this.state.lat){
+             return <div>latitude: {this.state.lat}</div>
+         }
+
+         return <div>Loading...</div>
+    }
+}
+
+ReactDOM.render(<App/>, document.querySelector('#root'));
